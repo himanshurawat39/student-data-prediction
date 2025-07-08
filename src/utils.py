cd1df0d6f72a77f60fbd4b0b613cbac80d1c4ec3
himanshurawat39 <himanshurawat4139@gmail.com>
2025-07-08 16:13:04 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 import os
 import sys
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+
 import pandas as pd
 import numpy as np
 from src.exception import CustomException
@@ -19,4 +23,33 @@ def save_object(file_path, obj):
         logging.info(f"Object saved at {file_path}")
 
     except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    """
+    This function evaluates the given models and returns a dictionary with model names as keys and their R2 scores as values.
+    """
+    try:
+        
+        logging.info("Evaluating models...")
+
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        logging.error("Error in model evaluation")
         raise CustomException(e, sys)
